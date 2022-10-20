5425c9595805b90b15c7ede9115eed6bea7bd78c
Diacryptic <diacryptic@gmail.com>
2022-10-20 12:30:00 +0100

    Refined Login Functionality
    The addition of a user file will allow the register page to add users.

--- signInCredentials.php
+++ signInCredentials.php
@@ -1,16 +1,39 @@
 <?php
-$email = "admin@make-it-all.com";
-$password = "\$2y\$10\$ZdPreyZwyuSEOedNetINUeicnlgMZC5zhBV2QoicUe3oGmyYFa39S";
 $emailInput = $_POST['emailInput'];
 $passwordInput = $_POST['passwordInput'];
 
-if(password_verify($passwordInput, $password) and $emailInput == $email){
-    echo "<script>window.alert(\"Correct Password\")</script>";
-    header("Location: /dashboard.html");
-    die();
-} else {
-    echo "<script>window.alert(\"Incorrect Password\")</script>";
-    header("Location: /index.html");
-    die(); 
+//Open Users text file and initialise users array
+$file = fopen("users.txt", "r") or die("Unable to find file!");
+$users_array = array();
+//////////////////////////////////////////////////
+
+//Getting all user details from users.txt
+while(!feof($file)) {
+    $email = trim(fgets($file));
+    $password = trim(fgets($file));
+    array_push($users_array, array($email, $password));
 }
+//////////////////////////////////////////
+
+
+//Searching if there is a user with matching email and password
+for ($i = 0; $i < count($users_array); $i++){
+    $email = $users_array[$i][0];
+    $password = $users_array[$i][1];
+
+    //If yes, redirect to dashboard
+    if(password_verify($passwordInput, $password) and $emailInput === $email){
+        echo "<script>window.alert(\"Correct Credentials\")</script>";
+        header("Location: /dashboard.php");
+        die();
+    }
+    ///////////////////////////////
+}
+/////////////////////////////////////////////////////////////////
+
+//If no matching user, redirect to sign in page
+echo "<script>window.alert(\"Incorrect Credentials\")</script>";
+header("Location: /index.html");
+die(); 
+///////////////////////////////////////////////
 ?>
