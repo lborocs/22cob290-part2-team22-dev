028ab48d96c1d340938ea0a81f0bdd149e9776fa
Jake Anderson <J.anderson2-21@student.lboro.ac.uk>
2022-10-29 13:32:19 +0100

    JS for editing task

--- navbar.php
+++ navbar.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['email'])){
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src = '../dashboard/navbar.js'></script>
+<script src = 'navbar.js'></script>
 <!--Bootstrap-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
--- productivity/processAddTaskForm.php
+++ productivity/processAddTaskForm.php
@@ -9,8 +9,14 @@
         $linkedEpic = $_POST["linkedEpic"];
         $assignee = $_POST["assignee"];
 		$current_data = file_get_contents('testProject.json');
-		$array_data = json_decode($current_data, true);
-        $new = array('taskName' => $taskName, 'taskStatus' => $taskStatus, 'linkedEpic' => $linkedEpic, 'assignee' => $assignee);
+        $array_data = json_decode($current_data, true);
+        if (!$array_data) {
+            $id = 1;
+        }
+        else {
+            $id = $array_data[count($array_data)-1]['id'] + 1;
+        }
+        $new = array('id' => $id,'taskName' => $taskName, 'taskStatus' => $taskStatus, 'linkedEpic' => $linkedEpic, 'assignee' => $assignee);
 		$array_data[] = $new;
 		$final_data = json_encode($array_data);
 		return $final_data;
