103ebeea093732447dbdb0210f0a2b3a857f27a6
Diacryptic <34571229+Diacryptic@users.noreply.github.com>
2022-11-05 13:20:14 +0000

    Update adminDashboard.php

--- dashboard/adminDashboard.php
+++ dashboard/adminDashboard.php
@@ -6,23 +6,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="dashboard/dashboard.js"></script>
-    <script>
-        function generateAuthCode(){
-            $.ajax({
-                    url:"dashboard/generateAuthCode.php",
-                    success: function(responseData){
-                        let temp = JSON.parse(responseData);
-                        document.getElementById("result").innerHTML = ("AuthCode Generated: <b>" + temp + "</b><br>Please share this with whoever you wish to invite.");
-                        document.getElementById("result").style = "color: green;"
-                    },
-                    error: function(e){
-                        document.getElementById("result").innerHTML = ("Error Occurred! Please refer to console.");
-                        console.log(e.message);
-                        document.getElementById("result").style = "color: red;"
-                    }
-                });
-        }
-    </script>
 </head>
 <body>
     <main class="bd-content p-5" id="content" role="main">
