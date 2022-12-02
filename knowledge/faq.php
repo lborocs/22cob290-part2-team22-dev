4d2afaa31118887f7e7fa0c5f3891bfebb794d14
Jake Anderson <jake1132569@gmail.com>
2022-12-02 11:38:47 +0000

    bootstrap added

--- knowledge/faq.php
+++ knowledge/faq.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="../style.css">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script>
     $(function(){
       $("#addFAQform").submit(function(event){
