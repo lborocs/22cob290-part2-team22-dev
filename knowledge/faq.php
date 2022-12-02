d93756846d2970bf741ca0c6448e67489dc6165a
Jake Anderson <jake1132569@gmail.com>
2022-12-02 13:01:21 +0000

    bootstrap consistency

--- knowledge/faq.php
+++ knowledge/faq.php
@@ -115,12 +115,6 @@
 <button id='addFAQ' type='button' data-bs-toggle='modal' data-bs-target='#addFAQModal' aria-controls='addFAQModal'>Add FAQ</button>
 </div>
 </body>
-<!--Bootstrap-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-<!--Bootstrap-->
 <script>
   
    $(document).ready(function(){
