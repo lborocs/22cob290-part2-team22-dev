2199ddbfad8c4d05840fd80c986d791d901198f7
Diacryptic <34571229+Diacryptic@users.noreply.github.com>
2023-02-09 10:43:20 +0000

    Standardising Bootstrap across pages
    Bootstrap 5.2 is now initially imported by the navbar only the navbar. Please don't include <head> tags in any of the additional pages or <body> tags as these have been removed to make it so only pure content from projects and wiki are imported into navbar. See the files and you'll know what I mean.
    
    Sizing of elements has been played around with also. I think it looks good atm but just import the files onto your sci-project and give it a look. LMK if anything breaks lol

--- navbar.php
+++ navbar.php
@@ -27,13 +27,12 @@ session_start();
 </script>
 
 </head>
-<body>
+<body style="width:100% !important;">
 <nav class="navbar navbar-dark" style="background-color: #FFB800; margin:0; border-radius:0;">
     <div class="container-fluid">
-        <a href="navbar.php" class="navbar-brand">
+        <a href="navbar.php" class="navbar-brand display-6 text-light">
             Make-It-All
         </a>
-        <div class="navbar-brand">Signed in as: <?php echo $_SESSION['email']?>.</div>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"  aria-controls="offcanvasNavbar" onclick="toggleClicked()">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -41,46 +40,44 @@ session_start();
         <!-- Offcanvas Navbar-->
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel"  data-bs-scroll="true" data-bs-backdrop="false" >
             <div class="offcanvas-header" style="background-color: #FFB800; height:3.5rem;">
-                <div></div>
+                <div class="display-6 text-light" style="font-size: 1.5rem;">Menu</div>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onclick="navShut()"></button>
             </div>
             <div class="offcanvas-body">
-                <h4 class="display-6" style="text-align: center;">Menu</h4>
-                <hr class="my-4">
                 <ul class="nav flex-column">
                     <li class="nav-item " style="text-align:center;">
                         <a id="dashboardlink" class="nav-link text-dark" onclick="<?php if ($_SESSION['isAdmin']==true) {echo "navclick('dashboard/adminDashboard.php')";} else {echo"navclick('dashboard/dashboard.php')";}?>">Dashboard</a>
                     </li>
+                    <hr class="my-2">
                     <li class="nav-item" style="text-align:center;">
                         <a id="projectlink" class="nav-link text-dark" onclick="navclick('productivity/projects.php')">Projects</a>
                     </li>
+                    <hr class="my-2">
                     <li class="nav-item " style="text-align:center;">
-                        <h4>Knowledge Wiki</h4>
-                        <a id="knowledgeNonTechnical" class="nav-link text-dark" onclick = "localStorage.setItem('technical', 0); navclick('knowledge/wiki.php'); localStorage.setItem('posts',0); location.reload();">Non-Technical Wiki</a>
-                        <a id="knowledgeTechnical" class="nav-link text-dark" onclick = "localStorage.setItem('technical', 1); navclick('knowledge/wiki.php'); localStorage.setItem('posts',0); location.reload();">Technical Wiki</a>
+                        <a id="knowledgeNonTechnical" class="nav-link text-dark" onclick = "localStorage.setItem('technical', 0); navclick('knowledge/wiki.php'); localStorage.setItem('posts',0);">Non-Technical Wiki</a>
+                        <a id="knowledgeTechnical" class="nav-link text-dark" onclick = "localStorage.setItem('technical', 1); navclick('knowledge/wiki.php'); localStorage.setItem('posts',0);">Technical Wiki</a>
                     </li>
                     <?php if($_SESSION['isAdmin'] == true) { echo(
-                        "<li class='nav-item ' style='text-align:center;'>
+                        "<hr class='my-2'><li class='nav-item ' style='text-align:center;'>
                             <a id='knowledgelink' class='nav-link text-dark' onclick =\"navclick('admin/viewUsers.php')\">View All Users</a>
                         </li>"
                     );}?>
                 </ul>
 
-            </div>
+            <hr class="my-2">
 
-            <hr class="my-4">
-
-            <div class="offcanvas-body">
-                <div>Please enter an appropriate email to send an invite to:</div>
-                <br>
+                <small class="text-muted">Please enter an appropriate email to send an invite to:</small>
                 <input type="email" class="form-control" id="emailInput" placeholder="Enter email">
                 <button class="btn btn-primary" style="margin-top:10px;" type="button" onclick="generateAuthCode()">Send Invite</button>
                 <div id="result" style="display:none"></div>
+
+            <hr class="my-2">
+            
+            <small class="text-muted">Signed in as:</small>
+            <small class="text-muted"><?php echo $_SESSION['email']?></small>
+                
             </div>
-
-            <hr class="my-4">
-
-            <div class="offcanvas-footer" style="text-align:center">
+            <div class="offcanvas-footer mb-3" style="text-align:center">
                 <a href="login/index.php" class="btn btn-alert">Sign Out</a>
             </div>
         </div>
--- productivity/projects.php
+++ productivity/projects.php
@@ -1,203 +1,185 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
+<script src="productivity/projects.js"></script>
+<script>
+    $(document).ready(function(){
+    
+        localStorage.setItem("currentPage", "/productivity/projects.php");
+    
+    });
+</script>
+<style>
 
-    <script src="../dashboard/navbar.js"></script>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0">
+    .tasks > .row {
+        min-height: 5rem;
+        padding-top: 1rem;
+        align-content: center;
+        display: block;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+    .tasks > .row > .card {
+        display: inline-block;
+    }
+</style>
 
-    <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="productivity/projects.js"></script>
-    <script>
-        $(document).ready(function(){
-        
-            localStorage.setItem("currentPage", "/productivity/projects.php");
-        
-        });
-    </script>
-    <style>
+<div class="row" style="margin-top:1%;">
+    <div class="col-sm-1 full-height" id="options">
+        <h6 id="selectedProject">No Project Selected.</h6>
+        <hr class="my-3">
+        <button class="btn btn-primary" style="margin-bottom:2%;" id="changeProjectButton" type="button" data-bs-toggle="modal" data-bs-target="#changeProjectModal" aria-controls="changeProjectModal">Change Selected Project</button>
+        <button class="btn btn-success" style="margin-bottom:2%;" id='addTaskButton' type="button" data-bs-toggle="modal" data-bs-target="#addTaskModal" aria-controls="addTaskModal">Add Task</button>
+        <button class="btn btn-primary" style="margin-bottom:2%;" id='filterTaskButton' type="button" data-bs-toggle="modal" data-bs-target="#filterTaskModal" aria-controls="filterTaskModal">Filter Tasks</button>
+    </div>
 
-        .tasks > .row {
-            min-height: 5rem;
-            padding-top: 1rem;
-            align-content: center;
-            display: block;
-            overflow-x: auto;
-            white-space: nowrap;
-        }
-        .tasks > .row > .card {
-            display: inline-block;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="row" style="margin-top:1%;">
-        <div class="col-sm-1 full-height" id="options">
-            <h5 id="selectedProject">No Project Selected.</h5>
-            <hr class="my-4">
-            <button class="btn btn-primary" style="margin-bottom:2%;" id="changeProjectButton" type="button" data-bs-toggle="modal" data-bs-target="#changeProjectModal" aria-controls="changeProjectModal">Change Selected Project</button>
-            <button class="btn btn-success" style="margin-bottom:2%;" id='addTaskButton' type="button" data-bs-toggle="modal" data-bs-target="#addTaskModal" aria-controls="addTaskModal">Add Task</button>
-            <button class="btn btn-primary" style="margin-bottom:2%;" id='filterTaskButton' type="button" data-bs-toggle="modal" data-bs-target="#filterTaskModal" aria-controls="filterTaskModal">Filter Tasks</button>
-        </div>
-
-        <div class="col-sm full-height">
-            <div id="displayTasks" style="display:none">
-                <h5>To Do</h5>
-                <div class="tasks">
-                    <div id="toDo" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                </div>
-                
-                <h5>Selected for Development</h5>
-                <div class="tasks">
-                    <div id="dev" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                </div>
-
-                <h5>In Progress</h5>
-                <div class="tasks">
-                    <div id="progress" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                </div>
-
-                <h5>Done</h5>
-                <div class="tasks">
-                    <div id="done" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                </div>
+    <div class="col-sm full-height p-2">
+        <div id="displayTasks" style="display:none">
+            <small>To Do</small>
+            <div class="tasks">
+                <div id="toDo" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
             </div>
-            <div id="noTasks" class="lead" style="margin-top: 27%;">
-                No Tasks to Show.
+            
+            <small>Selected for Development</small>
+            <div class="tasks">
+                <div id="dev" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+            </div>
+
+            <small>In Progress</small>
+            <div class="tasks">
+                <div id="progress" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+            </div>
+
+            <small>Done</small>
+            <div class="tasks">
+                <div id="done" class="row" style="margin:2%;" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+            </div>
+        </div>
+        <div id="noTasks" class="lead" style="margin-top: 27%;">
+            No Tasks to Show.
+        </div>
+    </div>
+</div>
+
+<div class="modal" id="changeProjectModal" tabindex="-1" role="dialog" aria-labelledby="changeProjectModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="changeProjectModalLabel">Select Which Project to View:</h5>
+            </div>
+
+            <div class="modal-body mx-auto">
+                <form id="changeProjectForm">
+                    <label for="ProjectNameField">Project Name:</label><br>
+                    <select class="form-control" id="ProjectNameField" name="ProjectNameField"></select><br>
+
+                    <button type="submit" class="btn btn-success">Choose this Project</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal" id="changeProjectModal" tabindex="-1" role="dialog" aria-labelledby="changeProjectModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="changeProjectModalLabel">Select Which Project to View:</h5>
-                </div>
+<div class="modal" id="addTaskModal" tabindex="-1" role="dialog" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addTaskModalLabel">Add a Task</h5>
+            </div>
 
-                <div class="modal-body mx-auto">
-                    <form id="changeProjectForm">
-                        <label for="ProjectNameField">Project Name:</label><br>
-                        <select class="form-control" id="ProjectNameField" name="ProjectNameField"></select><br>
+            <div class="modal-body">
+                <form id="addTaskForm">
+                    <div class="form-group">
+                        <label for="taskName">Input Task Name:</label>
+                        <input type="text" class="form-control" id="taskName" name="taskName" required>
+                    </div>
 
-                        <button type="submit" class="btn btn-success">Choose this Project</button>
-                    </form>
-                </div>
+                    <label for="taskStatus">Current Task Status:</label>
+                    <select class="form-control" name="taskStatus" id="taskStatus" required>
+                        <option value="0">To Do</option>
+                        <option value="1">Selected for Development</option>
+                        <option value="2">In Progress</option>
+                        <option value="3">Done</option>
+                    </select>
+                    <br>
+
+                    <div class="form-group">
+                        <label for="descriptionTextArea">Description:</label>
+                        <textarea class="form-control" id="descriptionTextArea" name="descriptionTextArea" rows="3"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="manHoursInput">Expected Man Hours to Completion:</label>
+                        <input type="number" class="form-control" name="manHoursInput" id="manHoursInput" min="0" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="assignee">Assignee:</label>
+                        <select class="form-control" id="assignee" name="assignee" required></select>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal" id="addTaskModal" tabindex="-1" role="dialog" aria-labelledby="addTaskModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addTaskModalLabel">Add a Task</h5>
-                </div>
 
-                <div class="modal-body">
-                    <form id="addTaskForm">
-                        <div class="form-group">
-                            <label for="taskName">Input Task Name:</label>
-                            <input type="text" class="form-control" id="taskName" name="taskName" required>
-                        </div>
+<div class="modal" id="EditTaskModal" tabindex="-1" role="dialog" aria-labelledby="EditTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="EditTaskModalLabel">Edit a Task</h5>
+            </div>
 
-                        <label for="taskStatus">Current Task Status:</label>
-                        <select class="form-control" name="taskStatus" id="taskStatus" required>
-                            <option value="0">To Do</option>
-                            <option value="1">Selected for Development</option>
-                            <option value="2">In Progress</option>
-                            <option value="3">Done</option>
+            <div class="modal-body">
+                <form id="EditTaskForm">
+                    <div class="form-group">
+                        <label for="editTaskName">Task Name:</label>
+                        <input type="text" class="form-control" id="editTaskName" name="editTaskName">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="editDescriptionTextArea">Description:</label>
+                        <textarea class="form-control" id="editDescriptionTextArea" name="editDescriptionTextArea" rows="3"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-success">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal" id="filterTaskModal" tabindex="-1" role="dialog" aria-labelledby="filterTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="filterTaskModalLabel">Add a Task</h5>
+            </div>
+
+            <div class="modal-body">
+                <form id="filterTaskForm">
+                    <div class="form-group">
+                        <label for="memberName">Team member to filter by:</label>
+                        <select multiple class="form-control" name="memberName" id="memberName" required>
+                            <option value = "None">None</option>
                         </select>
-                        <br>
-
-                        <div class="form-group">
-                            <label for="descriptionTextArea">Description:</label>
-                            <textarea class="form-control" id="descriptionTextArea" name="descriptionTextArea" rows="3"></textarea>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="manHoursInput">Expected Man Hours to Completion:</label>
-                            <input type="number" class="form-control" name="manHoursInput" id="manHoursInput" min="0" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="assignee">Assignee:</label>
-                            <select class="form-control" id="assignee" name="assignee" required></select>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
-
-    <div class="modal" id="EditTaskModal" tabindex="-1" role="dialog" aria-labelledby="EditTaskModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="EditTaskModalLabel">Edit a Task</h5>
-                </div>
-
-                <div class="modal-body">
-                    <form id="EditTaskForm">
-                        <div class="form-group">
-                            <label for="editTaskName">Task Name:</label>
-                            <input type="text" class="form-control" id="editTaskName" name="editTaskName">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="editDescriptionTextArea">Description:</label>
-                            <textarea class="form-control" id="editDescriptionTextArea" name="editDescriptionTextArea" rows="3"></textarea>
-                        </div>
-
-                        <button type="submit" class="btn btn-success">Save</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="modal" id="filterTaskModal" tabindex="-1" role="dialog" aria-labelledby="filterTaskModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="filterTaskModalLabel">Add a Task</h5>
-                </div>
-
-                <div class="modal-body">
-                    <form id="filterTaskForm">
-                        <div class="form-group">
-                            <label for="memberName">Team member to filter by:</label>
-                            <select multiple class="form-control" name="memberName" id="memberName" required>
-                                <option value = "None">None</option>
-                            </select>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script type="text/javascript">
-        changeSelected("project");
-        var myOffcanvas = document.getElementById('offcanvasNavbar')
-        myOffcanvas.addEventListener('hidden.bs.offcanvas', function() {
-            navShut()
-        })
-        GrabProjects();
-        GrabAssignees();
-        sessionStorage.removeItem("chosenProject");
-    </script>
-</body>
-
-</html>
+<script type="text/javascript">
+    changeSelected("project");
+    var myOffcanvas = document.getElementById('offcanvasNavbar')
+    myOffcanvas.addEventListener('hidden.bs.offcanvas', function() {
+        navShut()
+    })
+    GrabProjects();
+    GrabAssignees();
+    sessionStorage.removeItem("chosenProject");
+</script>
