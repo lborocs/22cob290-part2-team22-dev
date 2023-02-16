10714cc8fb085a14ea74b2b49c779b06a8ff3d1e
Charlie <cmartin030403@gmail.com>
2023-02-16 07:04:07 +0000

    Fixed some css

--- dashboard/adminDashboard.php
+++ dashboard/adminDashboard.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-   
+
     <title>Admin Dashboard</title>
     <link type="text/css" rel="stylesheet" href="/dashboard/dashboard.css" />
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -11,52 +12,55 @@
     <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
     <script src="dashboard/dashboard.js"></script>
     <script>
-         $(document).ready(function(){
-       
+        $(document).ready(function () {
+
             localStorage.setItem("currentPage", "dashboard/adminDashboard.php");
-        
-            
-        
+
+
+
         });
     </script>
 </head>
+
 <body style="width:100% !important;">
-    <main class="bd-content p-5" id="content" role="main">
+    <main class="bd-content" id="content" role="main">
         <div class="float-container">
             <div class="header">
-            <section class="jumbotron jumbotron-fluid" style="border-radius: 5px; padding: 5px;">
-                <h1 class="display-4" style="text-align: center;">Admin Dashboard</h1>
-            </section>
+                <section class="jumbotron jumbotron-fluid" style="border-radius: 5px; padding: 5px;">
+                    <h1 class="display-4" style="text-align: center;">Admin Dashboard</h1>
+                </section>
             </div>
             <div class="float-child" id="projectContainer">
-            <section class="jumbotron jumbotron-fluid" style="border-radius: 15px; padding: 20px;">
-                <h2 class="display-4" style="text-align: center;">Current Projects</h2>
+                <section class="jumbotron jumbotron-fluid" style="border-radius: 15px; padding: 20px;">
+                    <h2 class="display-4" style="text-align: center;">Current Projects</h2>
 
-                <hr class="my-4">
+                    <hr class="my-4">
 
 
-                <div class="row" id="AdminProjectOverview" style="overflow-x: hidden;">
-                    <script>GrabProjects();</script>
-                </div>
-                <button type="button" id="createProjectBtn" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#CreateProjectModal" aria-controls="CreateProjectModal">Create Project</button>
-            </section>
+                    <div class="row" id="AdminProjectOverview" style="overflow-x: hidden;">
+                        <script>GrabProjects();</script>
+                    </div>
+                    <button type="button" id="createProjectBtn" class="btn btn-info" data-bs-toggle="modal"
+                        data-bs-target="#CreateProjectModal" aria-controls="CreateProjectModal">Create Project</button>
+                </section>
             </div>
             <div class="float-child" id="toDo">
-            <section class="jumbotron jumbotron-fluid" style="border-radius: 15px; padding: 20px;">
-                <h5 class="display-4" style="text-align: center;">To Do List:</h5>
+                <section class="jumbotron jumbotron-fluid" style="border-radius: 15px; padding: 20px;">
+                    <h5 class="display-4" style="text-align: center;">To Do List:</h5>
 
-                <hr class="my-4">
+                    <hr class="my-4">
 
-                <div id="editor"></div>
+                    <div id="editor"></div>
 
-            </section>
-        </div> 
+                </section>
+            </div>
         </div>
 
-        
+
     </main>
 
-    <div class="modal" id="CreateProjectModal" tabindex="-1" role="dialog" aria-labelledby="CreateProjectModalLabel" aria-hidden="true">
+    <div class="modal" id="CreateProjectModal" tabindex="-1" role="dialog" aria-labelledby="CreateProjectModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -66,7 +70,8 @@
                 <div class="modal-body mx-auto">
                     <form id="CreateProjectForm">
                         <label for="ProjectNameField">Project Name:</label><br>
-                        <input type="text" class="form-control" id="ProjectNameField" name="ProjectNameField" required><br>
+                        <input type="text" class="form-control" id="ProjectNameField" name="ProjectNameField"
+                            required><br>
 
                         <label for="TeamLeaderField">Assign a Team Leader:</label><br>
                         <select class="form-control" id="TeamLeaderField" name="TeamLeaderField" required>
--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -17,10 +17,10 @@ function GrabProjects(){
             document.getElementById("AdminProjectOverview").innerHTML = "";
             let temp = JSON.parse(responseData);
             for (let project of temp){
-                let card = "<div class='card' style='width: 14rem; margin-left: 10px; margin-right: 10px;'>";
+                let card = "<div class='card' style='width: 23%; margin-left: 1%; margin-right: 1%;'>";
                 card += "<div class='card-body'>";
                 card += "<p class='card-title'>"+ project['projectName'] +"<p>";
-                card += "<p class='card-subtitle mb-2 text-muted'>Team Leader: " + project['teamLeader'] + "</p><hr class='my-1'>";
+                card += "<p class='card-subtitle mb-2 text-muted'>" + project['teamLeader'] + "</p><hr class='my-1'>";
                 card += "<button type='button' onclick='directToProject(\""+project['projectID']+"\", \""+project['projectName']+"\")' class='btn btn-primary'>Go to Project</button>";
                 card += "</div></div>";
 
