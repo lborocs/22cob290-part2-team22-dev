991866a3cbc69e72c965eb8260d2e16722d2e48e
Diacryptic <34571229+Diacryptic@users.noreply.github.com>
2023-02-16 15:51:45 +0000

    For Users...

--- productivity/databasePHPFiles/retrieveNonUserAssignedTaskCards.php
+++ productivity/databasePHPFiles/retrieveNonUserAssignedTaskCards.php
@@ -10,7 +10,8 @@ if (!$conn) {
 $projectID = $_POST['projectID'];
 $email = $_POST['email'];
 
-$sql = "SELECT taskID, taskName, status FROM tasks WHERE projectID = '$projectID' AND NOT EXISTS (SELECT * FROM taskToUserMapping WHERE email = '$email' AND tasks.projectID = taskToUserMapping.projectID AND tasks.taskID = taskToUserMapping.taskID)";
+$sql = "SELECT tasks.taskID, taskName, status, COUNT(email) as assignees, expectedManHours FROM tasks LEFT JOIN taskToUserMapping ON (tasks.taskID = taskToUserMapping.taskID AND taskToUserMapping.projectID = tasks.projectID) WHERE tasks.projectID = '$projectID' AND NOT EXISTS (SELECT * FROM taskToUserMapping WHERE email = '$email' AND tasks.projectID = taskToUserMapping.projectID AND tasks.taskID = taskToUserMapping.taskID) GROUP BY tasks.taskID";
+
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result)>0){
